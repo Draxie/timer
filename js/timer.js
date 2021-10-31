efa11aaef376a45731bb007eac89ce971e59026c
Draxie <darijeparezanovic@gmail.com>
2021-10-31 15:47:13 +0100

    Initial commit

---
+++ js/timer.js
@@ -0,0 +1,10 @@
+let progressCircle = document.querySelector(".progress");
+let radius = progressCircle.r.baseVal.value;
+let circumference = radius * 2 * Math.PI;
+progressCircle.style.strokeDasharray = circumference;
+
+setProgress(90);
+
+function setProgress(percent) {
+    progressCircle.style.strokeDashoffset = circumference - (percent / 100) * circumference;
+}
